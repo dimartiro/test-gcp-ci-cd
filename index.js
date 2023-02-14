6126d0264af0140ca3d815dee506876ef9eb9db4
Diego <diego2737@gmail.com>
2023-02-14 14:23:56 -0300

    Update function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World change 1!';
+    let message = req.query.message || req.body.message || 'Hello World change 2!';
     res.status(200).send(message);
   };
   
